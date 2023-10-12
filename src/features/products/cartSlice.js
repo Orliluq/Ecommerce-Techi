929ca1991a0bad9387d4559b002890cba48cd108
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-10-12 15:05:50 -0400

    Add files via upload

---
+++ src/features/products/cartSlice.js
@@ -0,0 +1,166 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+const initialState = {
+  cartItems: localStorage.getItem("cartItem")
+    ? JSON.parse(localStorage.getItem("cartItem"))
+    : [],
+
+  cartTotalAmount: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    // addToCart
+    addToCart(state, action) {
+      const existedItemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      // if exist
+      if (existedItemIndex >= 0) {
+        // increase quantity
+        state.cartItems[existedItemIndex].cartQuantity += 1;
+
+        //React Toastify added
+        toast.info("Quantity increased!", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      } else {
+        // add to cart
+        const assembledItem = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(assembledItem);
+
+        //React Toastify added
+        toast.success("Product added into cart!", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+      // add to local storage
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+
+    // remove from cart
+    removeFromCart(state, action) {
+      const updatedCartItems = state.cartItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+      state.cartItems = updatedCartItems;
+
+      //React Toastify added
+      toast.warn("Product removed from cart!", {
+        position: "bottom-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      // update local storage
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+
+    // clear cart
+    clearCart(state, action) {
+      state.cartItems = [];
+
+      //React Toastify added
+      toast.error("Cart cleared!", {
+        position: "bottom-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+
+      // update local storage
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+
+    // decrease cart
+    decreaseCart(state, action) {
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      //if exist
+      if (state.cartItems[itemIndex].cartQuantity > 1) {
+        state.cartItems[itemIndex].cartQuantity -= 1;
+
+        //React Toastify added
+        toast.info("Quantity decreased!", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      } else if (state.cartItems[itemIndex].cartQuantity === 1) {
+        const updatedCartItems = state.cartItems.filter(
+          (item) => item.id !== action.payload.id
+        );
+        state.cartItems = updatedCartItems;
+
+        //React Toastify added
+        toast.warn("Product removed from cart!", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+      // update local storage
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+
+    // get subtotal
+    getSubtotal(state, action) {
+      const Subtotal = state.cartItems.reduce((acc, item) => {
+        const { price, cartQuantity } = item;
+        const itemTotal = price * cartQuantity;
+        acc += itemTotal;
+
+        return acc;
+      }, 0);
+
+      state.cartTotalAmount = Subtotal;
+    },
+  },
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  clearCart,
+  decreaseCart,
+  getSubtotal,
+} = cartSlice.actions;
+export default cartSlice.reducer;
---
+++ src/pages/NotFound.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="container mx-auto mt-20 text-center">
+      <img
+        className="img h-46 w-64 mx-auto"
+        src="https://cdni.iconscout.com/illustration/premium/thumb/404-error-alert-5759424-4816343.png"
+        alt=""
+      />
+      <p className="text-4xl mb-4 font-semibold text-rose-600">
+        Page not found
+      </p>
+      <Link to="/">
+        <button className="bg-red-800 text-gray-50 px-6 py-3 uppercase tracking-widest font-medium rounded-lg hover:bg-violet-500 duration-300">
+          Go to home
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
