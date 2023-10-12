099ce2cfd1b900690e920d7156c819b5e538362f
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-10-12 15:05:13 -0400

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="footer bg-violet-700 text-violet-50 text-center py-10">
-      <p>&copy; {new Date().getFullYear()} Techi Copyright © 2023 Orlidev. All Rights Reserved <sup>❤️</sup></p>
-    </div>
-  );
-};
-
-export default Footer;
